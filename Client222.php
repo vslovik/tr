3d967377c26bda8e0cfd683ce534ddc4911da52b
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-07-22 13:49:59 +0200

    new texts to align

---
+++ Client222.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$in = fopen('data/carnage/en_clean', 'r');
+$out = fopen('data/carnage/en_clean_', 'w');
+$strs = [];
+while ($line = fgets($in)) {
+    if(empty(trim($line))) {
+            $str = implode(' ', $strs);
+            if($strs)
+                fputs($out, $str . "\n\n");
+            $strs = [];
+    } else {
+        if($line)
+            $strs[] = trim($line);
+    }
+}
+$str = implode(' ', $strs);
+fputs($out, $str . "\n\n");
+fclose($in);
+fclose($out);
