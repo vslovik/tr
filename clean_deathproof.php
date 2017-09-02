d1f919b230a063513ae36bf10cc48da2499870d9
Valeriya Slovikovskaya <vslovik@gmail.com>
2017-09-02 17:35:39 +0200

    new texts to align

---
+++ clean_deathproof.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$in = fopen('data/deathproof/it.srt', 'r');
+$i = 0;
+while ($line = fgets($in)) {
+    if(!empty($line) && intval(trim($line))) {
+        echo "\n";
+    }
+
+    else if (!empty(trim($line)) && $line && (0 !== substr_compare($line, '01', 0, 2) && substr_compare("00", $line, 0, 2) !== 0)) {
+        echo trim($line) . "\n";
+    }
+
+}
+fclose($in);
+
